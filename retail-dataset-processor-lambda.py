d282f8f3b080c709ce26ed358edbd036214fb8f3
anishbhateja <anishbhateja99@gmail.com>
2025-03-23 12:48:09 +0530

    Initial commit: Add data pipeline script and .gitignore

---
+++ retail-dataset-processor-lambda.py
@@ -0,0 +1,92 @@
+import json
+import base64
+import boto3
+from datetime import datetime
+
+
+def lambda_handler(event, context):
+    print(event)
+
+    # Read data from the incoming kinesis data stream events and print the data
+    for record in event['Records']:
+        data = base64.b64decode(record['kinesis']['data']).decode('utf-8-sig')
+        # print(data)
+        # print(type(data))
+        event_msg = json.loads(data)
+        print(event_msg)
+
+        invoice_num = str(event_msg["RetailTxnData"]["InvoiceNo"])
+        # print(event_msg["RetailTxnData"])
+        # print(type(invoice_num))
+
+        store_num = str(event_msg["RetailTxnData"]["StoreNo"])
+        region = str(event_msg["RetailTxnData"]["Region"])
+
+        print("invoice_num : {}".format(invoice_num))
+        print("store_num : {}".format(store_num))
+        print("region : {}".format(region))
+
+        print("region : {}".format(region)) # prashanth
+
+        print("store_num : {}".format(store_num)) # kiran
+
+        # processing_date = datetime.now().strftime("%Y-%m-%d")
+        # processing_hour = datetime.now().strftime("%H:%M:%S")[0:2]
+
+        invoice_processing_date = str(event_msg["RetailTxnData"]["InvoiceDate"])[:10]
+        invoice_processing_hour = str(event_msg["RetailTxnData"]["InvoiceDate"])[11:13]
+
+        # Use invoice date
+
+        print(invoice_processing_date)
+        print(type(invoice_processing_date))
+        print(invoice_processing_hour)
+        print(type(invoice_processing_hour))
+
+        file_name = "retail_dataset_{region}_{store_num}_{invoice_num}.json".format(region=region, store_num=store_num,
+                                                                                    invoice_num=invoice_num)
+        # file= "s3://retail-dataset-training/archive/region={region}/store-num={store_num}/processing_date={processing_date}/processing_hour={processing_hour}".format(region=region,store_num=store_num,processing_date=processing_date,processing_hour=processing_hour)
+        # s3://retail-dataset-training/archive/region=US/store-num=888/invoice_processing_date=2024-03-04/invoice_processing_hour=07/retail_dataset_US_888_084.json
+        # print("Archiving source msg to file : {}".format(file))
+
+        bucket_name = 'retail-dataset-training'
+        json_file_key = "archive/region={region}/store-num={store_num}/invoice_processing_date={invoice_processing_date}/invoice_processing_hour={invoice_processing_hour}/{file_name}".format(
+            region=region, store_num=store_num, invoice_processing_date=invoice_processing_date, invoice_processing_hour=invoice_processing_hour,
+            file_name=file_name)
+        # message_to_put = {'key': 'value', 'another_key': 'another_value'}
+
+        # Create an S3 client
+        s3 = boto3.client('s3')
+
+        # Convert the Python dictionary to a JSON string
+        json_message = json.dumps(event_msg)
+
+        # Put the JSON message into the S3 bucket
+        s3.put_object(Body=json_message, Bucket=bucket_name, Key=json_file_key)
+
+        print(f"Message successfully put into archive S3 file s3://: {bucket_name}/{json_file_key}")
+
+        delivery_stream_name = 'retail-dataset-firehose'
+
+        # Replace 'your_aws_access_key' and 'your_aws_secret_key' with your AWS credentials
+
+        aws_region = 'us-east-1'  # Replace with your AWS region
+
+        # Create a Kinesis Firehose client
+        firehose_client = boto3.client(
+            'firehose',
+            region_name=aws_region
+        )
+
+        # Sample record data (replace with your actual record data)
+        record_data = {
+            "Data": json_message
+        }
+
+        # Put record into the Kinesis Firehose delivery stream
+        response = firehose_client.put_record(
+            DeliveryStreamName=delivery_stream_name,
+            Record=record_data
+        )
+
+        print(f"Record put successfully. Response: {response}")
---
+++ test_data_generator.py
@@ -0,0 +1,60 @@
+import boto3
+import json
+import random
+import string
+
+
+def list_s3_objects(bucket_name, prefix):
+    """
+    List all S3 objects in a specified prefix.
+    """
+    s3 = boto3.client('s3')
+    response = s3.list_objects_v2(Bucket=bucket_name, Prefix=prefix)
+
+    obj_list = []
+    for obj in response['Contents']:
+        if obj['Key'].endswith('.json'):
+            obj_list.append(obj['Key'])
+
+    return obj_list
+
+def read_json_from_s3(bucket_name, key):
+    """
+    Read JSON data from a specified S3 object.
+    """
+    s3 = boto3.client('s3')
+    response = s3.get_object(Bucket=bucket_name, Key=key)
+    return json.loads(response['Body'].read().decode('utf-8'))
+
+def generate_random_string(length):
+    return ''.join(random.choices(string.ascii_letters + string.digits, k=length))
+def put_json_to_kinesis(data, stream_name):
+    """
+    Put JSON data to a Kinesis stream.
+    """
+    kinesis = boto3.client('kinesis')
+    data = json.dumps(data)
+    response = kinesis.put_record(StreamName=stream_name, Data=data, PartitionKey=generate_random_string(10))
+    print("Data put to Kinesis stream:", response)
+
+
+def main():
+    # Replace these values with your own
+    bucket_name = 'sparkwithdeepak-training-bucket-new'
+    prefix = 'test-data'
+    stream_name = 'test-kinesis_stream999'
+
+    # Step 1: List all S3 objects in the prefix
+    s3_objects = list_s3_objects(bucket_name, prefix)
+    print("S3 Objects in prefix:", s3_objects)
+
+    # Step 2: Read JSON data from each S3 object and put it to Kinesis stream
+    for s3_object in s3_objects:
+        json_data = read_json_from_s3(bucket_name, s3_object)
+        print("Data read from S3 object:", json_data)
+        for txn in json_data:
+            put_json_to_kinesis(txn, stream_name)
+
+
+if __name__ == "__main__":
+    main()
